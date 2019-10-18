b0480f37a77131e718051a836a638e13637b8087
d.amaxilatis <d.amaxilatis>
2019-10-18 14:05:59 +0300

    scan Docker processor

--- ipn-mapper/src/main/java/net/sparkworks/mapper/Application.java
+++ ipn-mapper/src/main/java/net/sparkworks/mapper/Application.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableConfigurationProperties
-@SpringBootApplication(scanBasePackages = {"net.sparkworks.mapper"})
+@SpringBootApplication(scanBasePackages = {"net.sparkworks.mapper", "net.sparkworks.common.processor"})
 public class Application {
     
     public static void main(final String[] args) {
