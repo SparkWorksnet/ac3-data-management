e604b4ee07c88b4e8aca0f2a50313f8be37d1c9d
amaxilat <d.amaxilatis@gmail.com>
2023-10-25 10:43:46 +0300

    update the video consumer code

--- video-streaming/video_producer_pika.py
+++ video-streaming/video_producer_pika.py
@@ -1,9 +1,3 @@
-"""Kombu-based Video Stream Publisher
-
-Written by Minsu Jang
-Date: 2018-06-09
-"""
-
 from __future__ import absolute_import, unicode_literals
 
 # import zlib as zip
@@ -15,33 +9,30 @@ import time
 import cv2
 import pika
 
-# Default RabbitMQ server URI
-
 credentials = pika.PlainCredentials('user', 'password')
-connection_parameters = pika.ConnectionParameters(host='127.0.0.1',
-                                                  port='5672',
-                                                  virtual_host='/',
+connection_parameters = pika.ConnectionParameters(host='127.0.0.1', port='5672', virtual_host='/',
                                                   credentials=credentials)
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
-# Video Capture by OpenCV
-capture = cv2.VideoCapture(0)
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+# This is the location the application will open to receive the video data
+# use '0', '1' or another index for a camera connected to this device
+# use the rtsp stream url for a remote feed
+inputstream = '0'
 
-while True:
+# Video Capture by OpenCV
+capture = cv2.VideoCapture(inputstream if not inputstream.isnumeric() else int(inputstream))
+
+while capture is not None:
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     ret, frame = capture.read()
     if ret is True:
-        # Make image smaller for faster delivery
         frame = cv2.resize(frame, None, fx=0.6, fy=0.6)
-        # Encode into JPEG
         result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-        # # Send JPEG-encoded byte array
         compressed_data = zip.compress(imgencode.tobytes())
         print(len(imgencode.tobytes()), len(compressed_data),
               len(zip.compress(imgencode.tobytes())) / len(imgencode.tobytes()))
         channel.basic_publish(exchange='video-exchange', routing_key='hello', body=compressed_data)
-
-    time.sleep(0.001)
+    time.sleep(0.0001)
 
 capture.release()
