50101c87e3969cddc35a46d0473ba76a5737565f
amaxilat <d.amaxilatis@gmail.com>
2024-02-19 16:52:55 +0200

    a test for sending a video feed through a RabbitMQ message broker

--- video-streaming/README.md
+++ video-streaming/README.md
@@ -1,9 +1,19 @@
+# Run the broker
+
+Run the broker using the following command: `docker compose -f docker-compose.yml up -d`
+
+This stack starts a RabbitMQ broker and opens access to it through port 5672. The default username/password
+is `guest`/`guest`.
+
 # Run the producer
 
 Run the `video_producer_pika.py` file.
 
-The program opens the computer's connected camera and starts receiving image frames.
+The program opens a video file and sends the video frames to the broker.
 Each frame is split in bytes and sent over the message queue.
+
+To send the computer's camera change the `inputstream` to `0`.
+To receive the video from an RTSP stream change the `inputstream` to the rtsp url.
 
 # Run the consumer
 
--- video-streaming/video_producer_pika.py
+++ video-streaming/video_producer_pika.py
@@ -10,19 +10,25 @@ import time
 import cv2
 import pika
 
-credentials = pika.PlainCredentials(os.getenv('BROKER_USERNAME'), os.getenv('BROKER_PASSWORD'))
-connection_parameters = pika.ConnectionParameters(host=os.getenv('BROKER_HOST'), port=os.getenv('BROKER_PORT'),
+credentials = pika.PlainCredentials(os.getenv('BROKER_USERNAME', "guest"), os.getenv('BROKER_PASSWORD', "guest"))
+connection_parameters = pika.ConnectionParameters(host=os.getenv('BROKER_HOST', "127.0.0.1"),
+                                                  port=os.getenv('BROKER_PORT', "5672"),
                                                   virtual_host=os.getenv('BROKER_VHOST', '/'),
                                                   credentials=credentials)
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
-exchange_name = os.getenv('BROKER_EXCHANGE')
-routing_key = os.getenv('BROKER_ROUTING_KEY', os.getenv('BROKER_EXCHANGE', 'video'))
 
-# This is the location the application will open to receive the video data
-# use '0', '1' or another index for a camera connected to this device
-# use the rtsp stream url for a remote feed
-inputstream = os.getenv('INPUT_STREAM')
+exchange_name = os.getenv('BROKER_EXCHANGE', "amq.topic")
+routing_key = os.getenv('BROKER_ROUTING_KEY', exchange_name)
+
+queue_name = os.getenv('BROKER_QUEUE', "output")
+
+channel.queue_declare(queue_name, passive=False, durable=True)
+channel.queue_bind(queue_name, exchange_name, "#")
+
+inputstream = os.getenv('INPUT_STREAM', "test.mp4")
+
+print(f"connecting to {inputstream}")
 
 # Video Capture by OpenCV
 capture = cv2.VideoCapture(inputstream if not inputstream.isnumeric() else int(inputstream))
