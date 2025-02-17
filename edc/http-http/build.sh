9fb06db845d3a7a2dea63e06555f6650c3b24af1
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2025-02-17 10:31:46 +0200

    update the build images

--- edc/http-http/build.sh
+++ edc/http-http/build.sh
@@ -1,14 +1,24 @@
-# build provider
+## build provider
 docker build -t ac3-connector-http-http-provider -f Provider.dockerfile .
 docker tag ac3-connector-http-http-provider sparkworks/ac3-connector-http-http-provider:latest
 docker push sparkworks/ac3-connector-http-http-provider:latest
 
-# build consumer
+## build iot-provider
+docker build -t ac3-connector-iot-http-http-provider -f ProviderIoT.dockerfile .
+docker tag ac3-connector-iot-http-http-provider sparkworks/ac3-connector-iot-http-http-provider:latest
+docker push sparkworks/ac3-connector-iot-http-http-provider:latest
+
+## build consumer
 docker build -t ac3-connector-http-http-consumer -f Consumer.dockerfile .
 docker tag ac3-connector-http-http-consumer sparkworks/ac3-connector-http-http-consumer:latest
 docker push sparkworks/ac3-connector-http-http-consumer:latest
 
-# build http-logger
+## build http-logger
 docker build -t ac3-http-request-logger -f HttpLogger.dockerfile .
 docker tag ac3-http-request-logger sparkworks/ac3-http-request-logger:latest
 docker push sparkworks/ac3-http-request-logger:latest
+
+## build amqp-http-logger
+docker build -t ac3-amqp-http-request-logger -f AmqpHttpLogger.dockerfile .
+docker tag ac3-amqp-http-request-logger sparkworks/ac3-amqp-http-request-logger:latest
+docker push sparkworks/ac3-amqp-http-request-logger:latest
